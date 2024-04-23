1a1a027e98feb58063cc542d7c0dd707297f556e
Shreya Chauk <shreyachauk@gmail.com>
2024-04-23 00:03:41 -0500

    integrated frontend with backend

--- load-db.py
+++ load-db.py
@@ -1,12 +1,10 @@
 import os
 import google.generativeai as genai
-import requests
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
-from io import BytesIO
 
 
 load_dotenv()
